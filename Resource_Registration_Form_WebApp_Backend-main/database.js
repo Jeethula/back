253ca98e55e144afd54d3846135b7bc5abe1b600
Jeethu_LA <113278334+Jeethula@users.noreply.github.com>
2023-10-04 12:38:28 +0530

    Add files via upload

---
+++ Resource_Registration_Form_WebApp_Backend-main/database.js
@@ -0,0 +1,20 @@
+const Sequlize = require('sequelize');
+require('dotenv').config();
+
+
+
+
+un = process.env.DB_USERNAME;
+pwd = process.env.DB_PASSWORD;
+
+
+const sequelize = new Sequlize("resource", un, pwd, {
+    dialect: "mysql",
+    logging: false,
+    timezone: '+05:30',
+    dialectOptions: {
+        useUTC: false //for reading from database
+    },
+
+}) 
+module.exports = sequelize
